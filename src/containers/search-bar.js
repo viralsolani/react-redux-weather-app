e4b2e4e749606d4a951cbec32b6d178ae0756491
Viral Solani <viral.solani@gmail.com>
2018-06-17 15:32:51 +0530

    add onSubmitEvent of form for search bar

--- src/containers/search-bar.js
+++ src/containers/search-bar.js
@@ -9,16 +9,20 @@ export default class SearchBar extends Component {
     this.state = { term: "" };
 
     this.onInputChange = this.onInputChange.bind(this);
-    //this.onFormSubmit = this.onFormSubmit.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
   }
 
   onInputChange(event) {
     this.setState({ term: event.target.value });
   }
 
+  onFormSubmit(event) {
+    event.preventDefault();
+  }
+
   render() {
     return (
-      <form onSubmit className="input-group">
+      <form onSubmit={this.onFormSubmit} className="input-group">
         <input
           placeholder="Get a five-day forecast in your favorite cities"
           className="form-control"

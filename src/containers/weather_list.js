001746644d1d3b86647e40dcf52d8e9555efaec0
Viral Solani <viral.solani@gmail.com>
2018-06-20 18:58:25 +0530

    Labeling of units on temperature graphs

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -7,13 +7,15 @@ class WeatherList extends Component {
   renderWeather(cityData) {
     const name = cityData.city.name;
     const temps = cityData.list.map(weather => weather.main.temp);
+    const pressures = cityData.list.map(weather => weather.main.pressure);
+    const humidities = cityData.list.map(weather => weather.main.humidity);
 
     return (
       <tr key={name}>
         <td>{name}</td>
-        <td>
-          <Chart data={temps} color="orange" />
-        </td>
+        <td><Chart data={temps} color="orange" units="K" /></td>
+        <td><Chart data={pressures} color="green" units="hPa" /></td>
+        <td><Chart data={humidities} color="black" units="%" /></td>
       </tr>
     );
   }
